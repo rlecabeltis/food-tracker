875cdf61d179dac1ad826355a936cbfc98f9bdb1
rlecabeltis <53131378+rlecabeltis@users.noreply.github.com>
2019-07-21 10:16:24 +0800

    Upload Folder
    Uploaded full food-tracker folder

---
+++ food_tracker/food/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from .views import index, add_meal, delete_meal, update_meal, view_meal
+
+urlpatterns = [
+	url(r'^$', index, name = 'index'),
+	url(r'^add_meal$', add_meal, name = 'add_meal'),
+	url(r'^delete_meal/(?P<meal_id>\d+)$', delete_meal, name = 'delete_meal'),
+	url(r'^update_meal/(?P<meal_id>\d+)$', update_meal, name = 'update_meal'),
+	url(r'^view_meal/(?P<meal_id>\d+)$', view_meal, name = 'view_meal'),
+]
